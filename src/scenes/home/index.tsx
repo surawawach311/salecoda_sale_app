1ac27ebea89f3351de205c4a8e8e361958572fd8
Surawach Saphapukdee <surawach311@gmail.com>
2020-11-05 10:33:05 +0700

    Add Simple Home

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text, TouchableHighlight } from "react-native";
+
+const HomeScreen = (navigation:any) => (
+  <SafeAreaView>
+    <Text>Screen: Home</Text>
+  </SafeAreaView>
+);
+
+export default HomeScreen;
