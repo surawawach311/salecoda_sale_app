ebcd5ddee7ffff2d8a39c5e525b2c637121e25b6
passapong <passapong.t@gmail.com>
2020-11-12 15:40:03 +0700

    fixed Capital Letter spelling

--- src/navigations/app-navigator.tsx
+++ src/navigations/app-navigator.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import InputTelNumberScreen from "../scenes/InputTelNumber/index";
+import InputTelNumberScreen from "../scenes/inputTelNumber/index";
 import InputOtp from "../scenes/InputOtp/index"
 const Stack = createStackNavigator();
 
