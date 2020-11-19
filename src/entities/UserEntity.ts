975f6e2c18346b5b0eb38b1c5684b0f79b38e882
surawach <surawach311@gmail.com>
2020-11-19 14:04:11 +0700

    Edit interface value

--- src/entities/UserEntity.ts
+++ src/entities/UserEntity.ts
@@ -1,16 +1,7 @@
 export interface UserEntity {
-    employee_id: number,
-    first_name: string,
-    last_name: string,
+    id: string,
+    name: string,
     telephone: string,
-    email1: string,
-    email2: string,
-    position_id: number,
-    position_name: string,
-    department_id: number,
-    department_name: string,
-    team_name: string,
-    company_id: number,
-    company_name_en: string,
-    company_name_th: string
+    email: string,
+    company: string,
 }
