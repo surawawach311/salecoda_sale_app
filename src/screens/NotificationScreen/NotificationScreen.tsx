7f5dd5349a3d15506c987fdd254966ae0e817b7b
surawach <surawach311@gmail.com>
2020-11-24 11:51:18 +0700

    New Screen

---
+++ src/screens/NotificationScreen/NotificationScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NotificationScreen: React.FC = () => {
+  return (
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+      <Text>OrderScreen</Text>
+    </View>
+  );
+};
+
+export default NotificationScreen
