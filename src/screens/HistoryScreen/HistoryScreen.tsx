f47002e7d9bec8c59c08a42d93415896bf23d779
surawach <surawach311@gmail.com>
2020-11-24 11:50:13 +0700

    Add new Screen for Example Navigator Bottom bar

---
+++ src/screens/HistoryScreen/HistoryScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const HistoryScreen: React.FC = () => {
+  return (
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+      <Text>HistoryScreen</Text>
+    </View>
+  );
+};
+
+export default HistoryScreen
