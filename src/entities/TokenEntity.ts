9a93692d0958a5b20294312b31a409ce2bb3f482
surawach <surawach311@gmail.com>
2020-11-27 17:26:08 +0700

    Feature: call data and render shop list

---
+++ src/entities/TokenEntity.ts
@@ -0,0 +1,3 @@
+export interface TokenEntity {
+    access_token: string
+}
--- src/screens/ShopListScreen/ShopListScreen.tsx
+++ src/screens/ShopListScreen/ShopListScreen.tsx
@@ -1,37 +1,70 @@
-import { Container, Content, List, ListItem ,Header} from "native-base";
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { List, ListItem, Text } from "native-base";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { ShopEntity } from "../../entities/ShopEntity";
+import { ShopFacade } from "../../facade/Shopfacade";
+import _ from "lodash";
+import { ScrollView } from "react-native-gesture-handler";
+import { AppLoading } from "expo";
 
 const ShopListScreen: React.FC = () => {
+  const [] = useState(false);
+  const [shopData, setShopData] = useState<
+    _.Object<_.Dictionary<ShopEntity[]>>
+  >();
+
+  useEffect(() => {
+    ShopFacade.getShopListData().then((res) => setShopData(res));
+  }, []);
+
+  const renderList: () => JSX.Element[] = () => {
+    const list: JSX.Element[] = [];
+    console.log(shopData);
+
+    if (shopData !== undefined) {
+      shopData.forEach((shops, territory) => {
+        list.push(
+          <>
+            <ListItem itemHeader>
+              <Text
+                style={styles.textHeader}
+              >{`ร้านค้าในเขต ${territory}`}</Text>
+            </ListItem>
+          </>
+        );
+        shops.map((shop) =>
+          list.push(
+            <ListItem>
+              <Text>{shop.name}</Text>
+            </ListItem>
+          )
+        );
+      });
+    } else {
+      <AppLoading/>
+    }
+    return list;
+  };
+
   return (
-    <Container>
-    {/* <Header /> */}
-    <Content>
-      <List>
-        <ListItem itemDivider>
-          <Text>A</Text>
-        </ListItem>                    
-        <ListItem>
-          <Text>Aaron Bennet</Text>
-        </ListItem>
-        <ListItem>
-          <Text>Ali Connors</Text>
-        </ListItem>
-        <ListItem itemDivider>
-          <Text>B</Text>
-        </ListItem>  
-        <ListItem>
-          <Text>Bradley Horowitz</Text>
-        </ListItem>
-      </List>
-    </Content>
-  </Container>
+    <View style={styles.container}>
+      <ScrollView>
+        <List>{renderList()}</List>
+      </ScrollView>
+    </View>
   );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // You should only need this
+    height: "100%", // But these wouldn't hurt.
+    width: "100%",
+  },
+  textHeader: {
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
+
 export default ShopListScreen;
