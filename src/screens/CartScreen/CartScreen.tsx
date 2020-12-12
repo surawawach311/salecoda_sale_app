aba139b34bbbb297229e3a3decdcdfb13b763f9d
surawach <surawach311@gmail.com>
2020-12-12 12:46:04 +0700

    Add props to product cart card and asset

--- src/screens/CartScreen/CartScreen.tsx
+++ src/screens/CartScreen/CartScreen.tsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import ButtonShop from "../../components/ButtonShop";
 import { StackScreenProps } from "@react-navigation/stack";
 import { PurchaseStackParamList } from "../../navigations/PurchaseNavigator";
 import ProductCartCard from "../../components/ProductCartCard";
 import InputNumber from "../../components/InputNumber";
+import { CartDataSource } from "../../datasource/CartDataSource";
+import { ScrollView } from "react-native-gesture-handler";
 
 type ShopScreenRouteProp = StackScreenProps<PurchaseStackParamList, "Cart">;
 
 const CartScreen: React.FC<ShopScreenRouteProp> = ({ navigation, route }) => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    getCart();
+  }, []);
+
+  const getCart = async () => {
+    CartDataSource.getCartByShop(route.params.shop.id).then((res) =>
+      setItems(res["items"])
+    );
+  };
+
   return (
     <View style={styled.container}>
       <View style={styled.warpChangeShop}>
         <ButtonShop onPress={() => navigation.navigate("ShopList")} />
       </View>
+
       <View style={styled.productContainer}>
         <Text>สินค้า</Text>
         <View style={styled.innerProductContainer}>
-          <ProductCartCard>
-            <InputNumber />
-          </ProductCartCard>
+          {items.map((item: any) => {
+            return (
+              <ProductCartCard
+                title={item.title}
+                image={encodeURI(item.image)}
+                pricePerUnit={item.price}
+                quantity={item.quantity}
+                priceTotal={item.total_price}
+              >
+                <InputNumber value={item.quantity.toString()} />
+              </ProductCartCard>
+            );
+          })}
         </View>
       </View>
     </View>
