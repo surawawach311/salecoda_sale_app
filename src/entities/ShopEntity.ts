054dcaefba20f39a1e9a3cd50af55cd1c8bd4967
surawach <surawach311@gmail.com>
2021-01-07 10:27:00 +0700

    Complete Delivery Method

--- src/entities/ShopEntity.ts
+++ src/entities/ShopEntity.ts
@@ -1,6 +1,11 @@
 export interface ShopEntity {
     id: string
     name: string
+    address: string
+    sub_district: string
+    district: string
+    province: string
+    post_code: string
     telephone: string
     territory: string
 }
