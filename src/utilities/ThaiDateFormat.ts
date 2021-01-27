5888c815d32decf4cca196a12e7c02c31306002e
surawach <surawach311@gmail.com>
2021-01-27 11:39:07 +0700

    add Thai time format

--- src/utilities/ThaiDateFormat.ts
+++ src/utilities/ThaiDateFormat.ts
@@ -1,11 +1,19 @@
-export const ThaiDateFormat = (datetime:any) =>{
-    const arr1 = datetime.split("-");
-    const arr2 = arr1[2].split(" ");
-    const date = new Date(arr1[0],arr1[1]-1,arr2[0]);
-    const result = date.toLocaleDateString("th-TH", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    });
-    return result
+export const ThaiDateFormat = (datetime: any) => {
+  const arr1 = datetime.split("-");
+  const arr2 = arr1[2].split(" ");
+  const date = new Date(arr1[0], arr1[1] - 1, arr2[0]);
+  const result = date.toLocaleDateString("th-TH", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  return result
 }
+
+export const ThaiTimeFormat = (datetime: any) => {
+  const arr1 = datetime.split("-");
+  const arr2 = arr1[2].split(" ");
+  const arr3 = arr2[1].split(":");
+  return arr3[0] + ":" + arr3[1] +"น."
+}
+
