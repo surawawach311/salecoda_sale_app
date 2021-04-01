a4132a78f3fc087916f4a5e0acc25b2be3a6f3fe
surawach <surawach311@gmail.com>
2021-04-01 11:29:38 +0700

    add empty

--- src/screens/NotificationScreen/NotificationScreen.tsx
+++ src/screens/NotificationScreen/NotificationScreen.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { Container } from "native-base";
 
 const NotificationScreen: React.FC = () => {
   return (
-    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
-      <Text>OrderScreen</Text>
-    </View>
+    <Container>
+      <View
+        style={{
+          alignContent: "center",
+          alignItems: "center",
+          marginTop: 200,
+          margin: 60,
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          style={{ height: 200, resizeMode: "contain" }}
+          source={require("../../../assets/under-construction.png")}
+        />
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: 50,
+          }}
+        >
+          <Text style={{ color: "#C2C6CE", fontSize: 14 }}>
+            อยู่ระหว่างการพัฒนาระบบ
+          </Text>
+          <Text style={{ color: "#C2C6CE", fontSize: 14 }}>
+            เตรียมพบกันเร็วๆนี้
+          </Text>
+        </View>
+      </View>
+    </Container>
   );
 };
 
-export default NotificationScreen
+export default NotificationScreen;
