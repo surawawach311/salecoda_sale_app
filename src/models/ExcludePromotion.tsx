e84df476c6210692c72e915fd3833d952833dc9d
Alongkon Insuwan <alongkon.i@iconkaset.com>
2021-07-26 20:20:56 +0700

    add exclude promotion

---
+++ src/models/ExcludePromotion.tsx
@@ -0,0 +1,12 @@
+export interface ExclusdePromotionModel {
+    item_id:string;
+    item_name: string;
+    promotion_id: string;
+    promotion_image?: string
+    promotion_name: string;
+    promotion_type:string;
+    checked?:boolean
+  }
+  
+ 
+  
