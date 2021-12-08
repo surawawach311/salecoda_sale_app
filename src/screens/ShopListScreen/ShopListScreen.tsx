7720c9436bae7188a5d47c45fbc577438052b09c
surawach <surawach311@gmail.com>
2021-12-08 13:24:32 +0700

    add empty state shop list

--- src/screens/ShopListScreen/ShopListScreen.tsx
+++ src/screens/ShopListScreen/ShopListScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { FlatList, StyleSheet, TouchableOpacity, View, Text } from 'react-native'
+import { FlatList, StyleSheet, TouchableOpacity, View, Image } from 'react-native'
 import { ApiShopEntity, ShopEntity } from '../../entities/ShopEntity'
 import { ShopFacade } from '../../facade/Shopfacade'
 import _ from 'lodash'
@@ -21,7 +21,7 @@ type ShopListScreenRouteProp = StackScreenProps<PurchaseStackParamList, 'ShopLis
 const ShopListScreen: React.FC<ShopListScreenRouteProp> = ({ navigation, route }) => {
   const [shopData, setShopData] = useState<ShopEntity[]>()
   const userDataStore = useContext(UserDataContext)
-  const { userData,selectShop } = userDataStore
+  const { userData, selectShop } = userDataStore
 
   const searchShop = (keywords: string) => {
     ShopDataSource.getShop(userData.territory, keywords).then((res: ResponseEntity<ApiShopEntity>) =>
@@ -39,43 +39,55 @@ const ShopListScreen: React.FC<ShopListScreenRouteProp> = ({ navigation, route }
     <View style={styles.container}>
       <CustomHeader title={'เลือกร้านค้า'} showBackBtn onPressBack={() => navigation.goBack()} />
       <View style={{ backgroundColor: 'white' }}>
-        <Search placeholder="ค้นหาร้านค้า" onChange={(e) => searchShop(e)} />
         <Search placeholder="ค้นหาร้านค้า..." onChange={(e) => searchShop(e)} />
       </View>
-      {shopData !== undefined && shopData.length > 0 ? (
-        <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
-          <View>
-            <View style={{ padding: 20, backgroundColor: '#F6F9FF' }}>
-              <Subheading2>{`ร้านค้าในเขต ${userData.territory}`}</Subheading2>
-            </View>
-            <FlatList
-              showsVerticalScrollIndicator={false}
-              data={shopData}
-              renderItem={({ item }) => (
-                <TouchableOpacity
-                  style={{ paddingTop: 20, paddingLeft: 10 }}
-                  key={item.id}
-                  onPress={() => {
-                    selectShop(item.shop_no)
-                    navigation.navigate('Brand', {
-                      shop: item,
-                      company: userData.company,
-                    })
-                  }}
-                >
-                  <Text1 style={{ paddingLeft: 20 }} key={item.name}>
-                    {item.name}
-                  </Text1>
-                  <View style={styles.line} />
-                </TouchableOpacity>
-              )}
-              keyExtractor={(item) => item.id}
-            />
-          </View>
-        </ScrollView>
-      ) : (
-        <AppLoading />
-      )}
+
+      <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}>
+        <View>
+          <FlatList
+            ListHeaderComponent={
+              <View style={{ padding: 20, backgroundColor: '#F6F9FF' }}>
+                <Subheading2>{`ร้านค้าในเขต ${userData.territory}`}</Subheading2>
+              </View>
+            }
+            showsVerticalScrollIndicator={false}
+            data={shopData}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{ paddingTop: 20, paddingLeft: 10 }}
+                key={item.id}
+                onPress={() => {
+                  selectShop(item.shop_no)
+                  navigation.navigate('Brand', {
+                    shop: item,
+                    company: userData.company,
+                  })
+                }}
+              >
+                <Text1 style={{ paddingLeft: 20 }} key={item.name}>
+                  {item.name}
+                </Text1>
+                <View style={styles.line} />
+              </TouchableOpacity>
+            )}
+            keyExtractor={(item) => item.id}
+            ListEmptyComponent={
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  alignContent: 'center',
+                  padding: 100,
+                }}
+              >
+                <Image style={styles.emptyImage} source={require('../../../assets/empty-state/shop.png')} />
+                <Text1 style={styles.emptyText}>ไม่พบร้านที่คุณต้องการ</Text1>
+              </View>
+            }
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
@@ -90,6 +102,14 @@ const styles = StyleSheet.create({
     borderBottomColor: '#EBEFF2',
     borderBottomWidth: 2,
     borderRadius: 3,
+  },
+  emptyImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  emptyText: {
+    color: '#C2C6CE',
   },
 })
 
